5d09e997d5ad4635a3954ac14ef65452fc00857d
jurandir <jurandir.gsh@gmail.com>
2020-07-06 18:14:35 -0300

    1a commit

---
+++ app/Controllers/Http/TweetController.js
@@ -0,0 +1,115 @@
+'use strict'
+
+const Response = require('@adonisjs/framework/src/Response');
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Tweet = use('App/Models/Tweet');
+
+/**
+ * Resourceful controller for interacting with tweets
+ */
+class TweetController {
+  /**
+   * Show a list of all tweets.
+   * GET tweets
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index () {
+    const tweets = await Tweet.all();
+    return tweets;
+
+  }
+
+  /**
+   * Render a form to be used for creating a new tweet.
+   * GET tweets/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new tweet.
+   * POST tweets
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, auth }) {
+    const data = request.only(['content']);
+    const tweet = await Tweet.create({ user_id: auth.user.id, ...data });
+
+    return tweet;
+
+  }
+
+  /**
+   * Display a single tweet.
+   * GET tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params }) {
+    const tweet = await Tweet.findOrFail(params.id);
+
+    return tweet;
+  }
+
+  /**
+   * Render a form to update an existing tweet.
+   * GET tweets/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update tweet details.
+   * PUT or PATCH tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a tweet with id.
+   * DELETE tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, auth , response }) {
+    const tweet = await Tweet.findOrFail(params.id);
+    if ( tweet.user_id != auth.user_id ) {
+      return response.status(401)
+    }
+
+    await tweet.delete();
+
+  }
+}
+
+module.exports = TweetController
